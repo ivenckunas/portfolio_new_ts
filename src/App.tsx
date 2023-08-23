d1496e4bbb7a1b44ba24b24e89d0958ae98f5a3c
ivenckunas <ignas.venckunas@gmail.com>
2023-08-23 13:26:45 +0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+	return (
+		<>
+			<h1 className='p-2 m-5 ml-5'>Hello</h1>
+			<p className='text-xs underline'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et, neque sequi quam dignissimos unde qui aliquid fugit cupiditate itaque impedit?</p>
+		</>
+	);
+}
+
+export default App;
