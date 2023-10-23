e88ae8323766ad4ecbd808bc731fac6145128542
ivenckunas <ignas.venckunas@gmail.com>
2023-10-23 14:01:50 +0300

    uppercase changed

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// src/main.js or src/main.ts
 import 'tailwindcss/tailwind.css';
-// Rest of your code follows...
-
 import App from './App.tsx';
 import './index.css';
 
