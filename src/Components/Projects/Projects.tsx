6a1680e1bd24e70b7206b661c58dc043b17637b6
ivenckunas <ignas.venckunas@gmail.com>
2023-11-30 13:17:50 +0200

    project additional link added

--- src/Components/Projects/Projects.tsx
+++ src/Components/Projects/Projects.tsx
@@ -20,6 +20,11 @@ function Projects() {
 		setCurrentProject(projectInArr);
 	};
 
+	const handleMouseClick = (id: number) => {
+		const projectLinkInArr = projectsArr[id].link;
+		window.open(projectLinkInArr, '_blank');
+	};
+
 	return (
 		<div>
 			<h2>projects</h2>
@@ -30,6 +35,7 @@ function Projects() {
 						className={id === 4 ? 'border-t-[1px] border-black ' : 'border-t-[1px] lg:mr-5 border-black'}
 					>
 						<span
+							onClick={() => handleMouseClick(project.id)}
 							onMouseEnter={() => handleMouseEnter(project.id)}
 							className='hover:text-[#FF0000] cursor-pointer inline-flex items-center py-3'
 						>
