3bd0ce5de84aad625e3a0e13b11ca866c9e80953
ivenckunas <ignas.venckunas@gmail.com>
2024-04-05 13:56:25 +0300

    location update

--- src/Components/About/About.tsx
+++ src/Components/About/About.tsx
@@ -17,7 +17,7 @@ function About() {
 		<div className='flex flex-col h-auto lg:pr-5 '>
 			<h2>hi, i'm</h2>
 			<h1 className='p-2 pl-0 text-2xl border-y-[1px] border-black uppercase'>ignas ven</h1>
-			<p className='p-2 lg:pt-4 pl-0 text-xs lg:text-sm'>My name is Ignas Venckunas, a front-end developer based in Vilnius, Lithuania. I'm all about creating awesome websites with React, Redux, TypeScript, SCSS, Material UI, Tailwinds CSS. My portfolio is where you'll find my work, and I'm on the lookout for cool opportunities to collaborate(or get hired) and help you bring fresh ideas to the web. </p>
+			<p className='p-2 lg:pt-4 pl-0 text-xs lg:text-sm'>My name is Ignas Venckunas, a frontend developer based in Alytus, Lithuania. I'm all about creating awesome websites with React, Redux, TypeScript, SCSS, Material UI, Tailwinds CSS. My portfolio is where you'll find my work, and I'm on the lookout for cool opportunities to collaborate(or get hired) and help you bring fresh ideas to the web. </p>
 
 			<img
 				className='h-[20vh] lg:h-[10vh] mb-5 object-cover'
